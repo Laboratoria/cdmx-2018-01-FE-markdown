ebee2f863ab51fc29e9413a946ae1912206239c7
GenesisMauries <exodo_54@hotmail.com>
2018-08-28 16:07:52 -0500

    Agrega prueba de test

---
+++ index.js
@@ -0,0 +1,9 @@
+let add = (a,b) => {
+    return a + b
+}
+
+
+
+module.exports = {
+    add
+}
---
+++ test.spec.js
@@ -0,0 +1,8 @@
+let util = require('./index'); // Trae el archivo que se debe testear
+
+describe('add function', () =>{
+    it ('add',()=>{
+        expect(true).toBe(true);
+        // expect espera que lo que esta entre parentesis sea igual a toBe y sus parentesis
+    })
+});
