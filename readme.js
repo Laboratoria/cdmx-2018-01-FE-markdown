ee494fe9910665ef1ecb289060f758733a6f11bf
GenesisMauries <exodo_54@hotmail.com>
2018-08-29 18:20:01 -0500

    Elimina codigo comentado

--- readme.js
+++ readme.js
@@ -1,27 +1,14 @@
-// let add = (a,b) => {
-//     return a + b
-// }
-
-
-
-// // Entra al README.md
-// let http = require('http'), // require 
-// fs = require('fs'); // fs encargado de comunicarse con el sistema de archivos de la computadora.
-// http.createServer((req,res)=>{
-// fs.readFile('./README.md', callback = (err,md) =>{
-//     res.write(md);
-//     res.end();
-// })  
-// }).listen(8080);
-let fs = require('fs');
-
+let fs = require("fs");
+// Funcion encargada de leer
 const readFile = () =>{
     fs.readFile('./README.md', 'utf-8',callback = (err, md)=>{
-        if (err) throw err
-        console.log(md);
+        if (err) {
+            console.log('Tienes un error, verifica'); 
+        }else {
+         console.log(md);   
+        }
     })
 }
-
 readFile();
 
 module.exports = {
