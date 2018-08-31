aa85beb3053161bed018ea79fd9ed76c66fffb4c
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-08-31 13:35:46 -0500

    subiendo refactoriazión de la primera función

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const path = require('path');
 /* readFile, método asíncrono, recibe 3 parámetros. 1 es la ruta del archivo que queremos leer, el segundo es un string con el juego de
  caracteres en el que el archivo está codificado y el 3ro es una callback que se ejecutará en el momento en el que el archivo esta leído
 y se encuentra disponible para hacer alguna cosa */ 
-fs.readFile('./README.md', 'utf8', (error, data) => {
+
+const data = (error, data) => {
   if (error) {
     console.log('error:', error);
   } else { 
     let link = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/g;
     console.log(data.match(link));
-    console.log(path.isAbsolute('./README.md'));
+    // console.log(path.isAbsolute('./README.md'));
   };
-});
+};
+
+fs.readFile('./README.md', 'utf8', data);
 
 const mdLinks = () =>{
   return 'holi';
@@ -21,4 +24,4 @@ const mdLinks = () =>{
 
 module.exports = mdLinks;
 
-// coment
+
