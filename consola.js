6cc87491c64fd9f43716f851ea6b10b76b5dad2b
AngieMonroe <ame.monroy@gmail.com>
2018-08-31 13:25:52 -0500

    Función para encontrar links y texto etiqueta a por medio del JSDOM

--- consola.js
+++ consola.js
@@ -1,1 +1,9 @@
 console.log('hola mundo');
+
+// Busca los links que se encuentran en el readme por medio de una expresión regular.
+const searchLink = (data) =>{
+  let rex = /((http:\/\/|https:\/\/|www\.)[^\s]+)/gim;
+  let rex2 = /\[(a-z)]/g;
+  let links = data.match(rex2);
+  console.log(links);
+};
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const resolve = require('path').resolve;
 const fs = require('fs');
-var url = require('url');
 const file = './README.md';
+const md = require('markdown-it')();
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
 
 // Verifica si la ruta es absoluta o relativa; si es relativa la convierte en absoluta.
 const route = (file) => {
@@ -12,7 +14,6 @@ const route = (file) => {
   } else {
     routeFile = resolve(file);
   }
-  console.log(routeFile)
   return routeFile;
 };
 
@@ -29,12 +30,29 @@ const readFile = (callback) => {
     callback(data);
   });
 };
-readFile(callback = data => searchLink(data));
+readFile(callback = data => renderData(data));
 
 // Busca los links que se encuentran en el readme por medio de una expresión regular.
 const searchLink = (data) =>{
   let rex = /((http:\/\/|https:\/\/|www\.)[^\s]+)/gim;
-  // let rex2 = /\[.\]/
+  let rex2 = /\[(a-z)]/g;
   let links = data.match(rex);
   console.log(links);
 };
+
+// Convierte la data en formato HTML
+const renderData = (data) => {
+  let dataHtml = md.render(data);
+  searchLinks(dataHtml);
+};
+
+// La Data se envía a la función que buscara los links con ayuda de JSDOM encuentro link y contenido de la etiqueta a
+const searchLinks = (dataHtml) => {
+  const dom = new JSDOM(`${dataHtml}`);
+  let tags = dom.window.document.querySelectorAll('a[href]');
+  for (let i = 0; i < tags.length; i++) {
+    let text = tags[i].textContent;
+    let href = tags[i].href;
+  }
+};
+
