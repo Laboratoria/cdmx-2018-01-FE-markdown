c7bacfe3b6f6b626e42464e88cdcfd3e22ff22c4
Josce Jaimes <nanayhachi94@gmail>
2018-09-03 14:35:53 -0500

    Subiendo segunda función

---
+++ src/README.md
@@ -0,0 +1,1 @@
+cuando yo era una niña me di cuenta que no se podia hacer nada con la muerte
--- src/main.js
+++ src/main.js
@@ -1,36 +1,40 @@
 // aqui se va a implementar las funciones
 
 const files = require('fs');
+//llamo a la api
 const path = require('path');
 
-
-const validateUrl = (url)=>{
-    if(path.isAbsolute(url) != true) {
-        console.log(url);
-        let urlResult = path.resolve(url)
+//definiendo una ruta absolutas
+const validatePath = (pathrout)=>{
+    console.log(typeof pathrout);
+    if(!path.isAbsolute(pathrout)) {
+        const urlResult = path.resolve(pathrout)
          console.log( urlResult);
     }
 }
-validateUrl();
 
-files.readFile('README.md', 'utf8')
+validatePath('README.md');
+
  const lineCount = (err, data) =>{
     if (err){
         console.log(err);
         return 'error';
     }else{
-        let lines = data.split('\n').length
+        //saltos de linea \n
+        const lines = data.split('\n').length
+        console.log(data);
         console.log(lines + ' ' +'lines')
         return lines + ' '+'lines'
         
-    }
-}
+    };
+};
+
 files.readFile('README.md', 'utf8',lineCount)
 
 
 module.exports = {
     lineCount, 
-    validateUrl
+    validatePath
 }; 
 
 
