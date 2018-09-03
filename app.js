e22ecfa3176fdd748674072607ce45fcc49c7ad6
Judith <baruki110214@gmail.com>
2018-09-03 00:17:09 -0500

    validando tipo de archivo que se lee

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-var http = require("http");
+const fs = require('fs');
+const path = require('path');
 
-var manejador = function(solicitud,respuesta){
-console.log("recibimos una nueva peticion");
-respuesta.end("hola mundo");
+const rutaDoct = (data) => {
+  // data es la ruta relativa del archivo README.md
+  // console.log(data);
+  if (!path.isAbsolute(data)) {
+    let fileAbsolut = path.resolve(data);
+    // console.log(fileAbsolut);
+    fs.readFile(fileAbsolut, 'utf8', function(err, data) {
+      if (err) {
+        // console.log('no hay archivo');
+      } else {
+        let lines = data.split('\n');
+        // console.log(lines);
+        for (let i = 0; i < lines.length; i++) {
+          let lineaPorLinea = lines[i];
+          console.log(lineaPorLinea);
+        }
+      }
+    });
+  }
 };
-//decirle en que puerto se ejecuta
-//retorna un objeto 
-var servidor = http.createServer(manejador);
-servidor.listen(8080);
 
+rutaDoct('./README.md');
--- app_html.js
+++ app_html.js
@@ -1,11 +1,51 @@
+
 var http = require("http"),
-fs = require("fs");
+var fs = require("fs");
+var ruta = require("path");
+
 //guardamos en una variable lo que retorna
-fs.readFile("./index.html", function(err,html){
-    http.createServer(function(req,res){
-        res.write(html);
-        res.end();
-        }).listen(8080);
-});
+//fs.readFile("./index.html", function(err,html){
+  //  http.createServer(function(req,res){
+    //    res.write(html);
+      //  res.end();
+       // }).listen(8080);
+//});
+
+const ejecutar=()=>{
+    const onRequest=(req,res)=>{
+        var rutaArchivo='.' + ((req.url == '/')?'/index.htm':req.url);
+        console.log("rutaArchivo");
+
+        var ext=ruta.extname(rutaArchivo);
+        var contentType = 'text/html';
+        switch(ext){
+            case '.css':
+            contentType = 'text/css';
+            break;
+            case '.js':
+            contentType = 'text/javascript';
+            break;
+        }
+        ruta.exist(rutaArchivo, e = (existe)=>{
+            if(existe){
+                fs.readFile(rutaArchivo, e =(error,contenido)=>{
+                    if(error){
+                        res.writeHead(500);
+                        res.end();
+                    }
+                    else{
+                        res.writeHead(200,{'content-Type':contentType});
+                        res.end(contenido);
+                    }
+                })
+            }else{
+                res.writeHead(404);
+                res.end();
+            }
+        });
+var server = http.creatServer().listen(3030);
+console.log("servidor corriendo en puerto 3030")
+        }
+ exports.ejecutar = ejecutar;
 
 
