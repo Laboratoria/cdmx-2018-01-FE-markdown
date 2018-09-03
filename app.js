3f375972f9a3177d70189fc0ff0ff981dfe065a9
Judith <baruki110214@gmail.com>
2018-09-03 00:26:13 -0500

    cantidad de lineas

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const path = require('path');
 
 const ruta = (data) => {
   if (!path.isAbsolute(data)) {
-    let fileAbsolut = path.resolve(data);
-  
+
+    const fileAbsolut = path.resolve(data);
     fs.readFile(fileAbsolut, 'utf8', function(err, data) {
-      if (err) {
+      
+      if (err){
         console.log('sin archivo');
-      } else {
+      } 
+      else{
         const line = data.split('\n');
         for (let i = 0; i < line.length; i++) {
           const lines = line[i];
-         // console.log();
-        }
-      }
+          console.log(lines); }
+                                               }
     });
   }
 };
