14dc11c8f18e072f60f85aa743d1916fa4f3f138
Alterna87 <fvelueta@outlook.com>
2018-09-03 18:08:41 +0000

    Separated files from index

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-// Algoritmo de prueba
+
+
+const { mdLink } = require('./js/md-links.js');
+
 //"jsdom": "^12.0.0"
-const fs = require('fs');
-const marked = require('marked');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
-const readfil =(callback)=> {
-  fs.readFile('./textprob.md', 'utf8', (err, data) => {
-    if (err) throw err
-    callback(data)
+let path = './textprob.md';
 
-  });
-}
-
-let data = fs.readFileSync('./textprob.md', 'utf8');
-const tokens = marked.lexer(data);
-const html = marked.parser(tokens);
-//console.log(html);
-const dom = new JSDOM(html);
-let links = dom.window.document.querySelectorAll('a');
-
-for (var i = 0; i < links.length; i++) {
-  console.log(links[i].href);
-  console.log(`Nombre del links: ${links[i].textContent }`);
-  }
-
-  console.log(`Hay ${ links.length } Links en el documento`);
+mdLink(path)
+.then(archivo =>  console.log(archivo));
+  // console.log(mdLink('./textprob.md'));
---
+++ src/js/md-links.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const marked = require('marked');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const mdLink = (path) => {
+// Retornaremos una promesa con la funciónote
+
+return new Promise((resolve, reject) => {
+
+
+const data = fs.readFileSync(path, 'utf8');
+const tokens = marked.lexer(data);
+const html = marked.parser(tokens);
+const dom = new JSDOM(html);
+let links = dom.window.document.querySelectorAll('a');
+
+let dataLinks = {};
+let arrayData = [];
+for (let i = 0; i < links.length; i++) {
+    dataLinks = {
+    href : links[i].href,
+    text: links[i].textContent,
+    file: path
+  }
+
+  arrayData.push(dataLinks);
+  }
+
+  console.log(arrayData);
+
+
+
+});
+};
+
+module.exports = {
+  mdLink
+}
