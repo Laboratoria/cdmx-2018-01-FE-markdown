d59869d5815b289c4f81d3e26710a770f8b26185
Judith <baruki110214@gmail.com>
2018-09-03 00:21:40 -0500

    modulo cantidad de lineas

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-const rutaDoct = (data) => {
-  // data es la ruta relativa del archivo README.md
-  // console.log(data);
+const ruta = (data) => {
   if (!path.isAbsolute(data)) {
     let fileAbsolut = path.resolve(data);
-    // console.log(fileAbsolut);
+  
     fs.readFile(fileAbsolut, 'utf8', function(err, data) {
       if (err) {
-        // console.log('no hay archivo');
+        console.log('sin archivo');
       } else {
-        let lines = data.split('\n');
-        // console.log(lines);
-        for (let i = 0; i < lines.length; i++) {
-          let lineaPorLinea = lines[i];
-          console.log(lineaPorLinea);
+        const line = data.split('\n');
+        for (let i = 0; i < line.length; i++) {
+          const lines = line[i];
+         // console.log();
         }
       }
     });
   }
 };
 
-rutaDoct('./README.md');
+ruta('./README.md');
