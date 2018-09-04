c0d8c5b1100e3bc6ebebc2067087760a4008f364
Your Name <you@example.com>
2018-09-03 23:42:51 -0500

    yargs add options

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const argv = require('yargs')
+.command('options', 'Opciones para validar los links', {
+  validate: {
+    alias: 'v'
+  }
+
+})
+.argv;
 
 
 const { mdLink } = require('./js/md-links.js');
@@ -5,9 +13,9 @@ const { mdLink } = require('./js/md-links.js');
 //"jsdom": "^12.0.0"
 
 let path = './textprob.md';
+console.log(argv);
 
 //Primera función
-mdLink(path)
-.then(archivo =>  console.log(archivo))
-.catch(console.error());
-  // console.log(mdLink('./textprob.md'));
+// mdLink(path)
+// .then(archivo =>  console.log(archivo))
+// .catch(console.error());
