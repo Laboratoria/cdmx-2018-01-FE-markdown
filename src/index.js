3705cab6345a6c5aff9873d0cace109d075acef2
Your Name <you@example.com>
2018-09-04 01:00:42 -0500

    add options yarg

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 const argv = require('yargs')
 .command('options', 'Opciones para validar los links', {
   validate: {
-    alias: 'v'
+    alias: 'va'
+  },
+  stats : {
+    alias: 'sta'
   }
-
 })
 .argv;
-
 
 const { mdLink } = require('./js/md-links.js');
 
 //"jsdom": "^12.0.0"
-
+let options = 'default';
 let path = './textprob.md';
-console.log(argv);
+;
+
+if (argv.validate) {
+  console.log('argv: validate');
+} else if (argv.stats) {
+  console.log('argv: stats');
+} else {
+  mdLink(path)
+  .then(archivo =>  console.log('Option de default'))
+  .catch(console.error());
+}
 
 //Primera función
 // mdLink(path)
--- src/js/md-links.js
+++ src/js/md-links.js
@@ -3,33 +3,31 @@ const marked = require('marked');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const mdLink = (path) => {
-// Retornaremos una promesa con la funciónote
+const mdLink = (path, options, err) => {
+// Retornaremos una promesa con la función
 
 return new Promise((resolve, reject) => {
+    if (err) reject()
+    else {
+    resolve();
+    const data = fs.readFileSync(path, 'utf8');
+    const tokens = marked.lexer(data);
+    const html = marked.parser(tokens);
+    const dom = new JSDOM(html);
+    let links = dom.window.document.querySelectorAll('a');
+    let dataLinks = {};
+    let arrayData = [];
+    for (let i = 0; i < links.length; i++) {
+        dataLinks = {
+        href : links[i].href,
+        text: links[i].textContent,
+        file: path
+      }
+      arrayData.push(dataLinks);
+      }
 
-const data = fs.readFileSync(path, 'utf8');
-const tokens = marked.lexer(data);
-const html = marked.parser(tokens);
-const dom = new JSDOM(html);
-let links = dom.window.document.querySelectorAll('a');
-
-let dataLinks = {};
-let arrayData = [];
-for (let i = 0; i < links.length; i++) {
-    dataLinks = {
-    href : links[i].href,
-    text: links[i].textContent,
-    file: path
+      console.log(arrayData);
   }
-
-  arrayData.push(dataLinks);
-  }
-
-  console.log(arrayData);
-
-
-
 });
 };
 
