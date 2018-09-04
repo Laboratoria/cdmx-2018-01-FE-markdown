9c5db0b57bba3d95f605209710bb42edd26cfb46
GenesisMauries <exodo_54@hotmail.com>
2018-09-04 16:24:44 -0500

    SUbe obtencion de array de objetos

--- test.spec.js
+++ test.spec.js
@@ -1,8 +1,15 @@
 let util = require('./readme.js').default; // Trae el archivo que se debe testear
 
-describe('readFile function', () =>{
-    it ('readFile ',()=>{
-        expect(true).toBe(true);
-        // expect espera que lo que esta entre parentesis sea igual a toBe y sus parentesis
-    })
+describe('readFile is a function', () =>{
+  it('readFile ', ()=>{
+    expect(true).toBe(true);
+    // expect espera que lo que esta entre parentesis sea igual a toBe y sus parentesis
+  });
+});
+
+describe('runArray is a function', () =>{
+  it('runArray ', ()=>{
+    expect(true).toBe(true);
+    // expect espera que lo que esta entre parentesis sea igual a toBe y sus parentesis
+  });
 });
