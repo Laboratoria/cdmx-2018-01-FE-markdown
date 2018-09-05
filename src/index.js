f68546cdd7925816f712bec003673e44db2907d2
Alterna87 <fvelueta@outlook.com>
2018-09-05 18:23:14 +0000

    fixed yargs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,65 @@
+#!/usr/bin/env node
 
 
-const { mdLink } = require('./js/md-links.js');
+const { mdLink, validateRoute } = require('./js/md-links');
+const yargs = require('yargs');
+
+let commands = yargs
+  .command({
+    command: '<path>',
+    strict: 'true'
+  })
+  .demandCommand(1, 1, 'ERROR: You need a path to run mdLinks', 'ERROR: Write the path without spaces')
+  .options({
+    '_': {
+      type: 'string',
+      description: 'Path to search markdown in',
+      demandOption: 'true',
+    },
+    'validate': {
+      type: 'boolean',
+      default: 'false',
+      description: 'Valida los links de un archivo markdown'
+    },
+    'stats': {
+      type: 'boolean',
+      default: 'false',
+      description: 'Muestra estadisticas básicas sobre los links encotrados en el archivo markdown'
+    }
+  })
+  .help()
+  .check((argv)=>{
+    if (Object.keys(argv).length <= 4) {
+      return true;
+    } else {
+      throw (new Error(' Opcion invalida'));
+    }
+  })
+  .argv;
 
 //"jsdom": "^12.0.0"
+console.log(commands);
 
-let path = './textprob.md';
 
-//Primera función
-mdLink(path)
-.then(archivo =>  console.log(archivo))
-.catch(console.error());
+let path = commands._[0];
 
-// Segunda función
+validateRoute(path);
+console.log(path);
+
+
+//'./textprob.md';
+
+// if (command == 'validate') {
+//   console.log('argv: validate');
+// } else if (command == 'stats') {
+//   console.log('argv: stats');
+// } else {
+//   mdLink(path)
+//   .then(archivo =>  console.log('Option de default'))
+//   .catch(console.error());
+// }
+//
+// // Primera función
+// mdLink(path)
+// .then(archivo =>  console.log(archivo))
+// .catch(console.error());
--- src/js/md-links.js
+++ src/js/md-links.js
@@ -2,33 +2,46 @@ const fs = require('fs');
 const marked = require('marked');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const mdLink = (path) => {
+const resolve = require('path').resolve;
+
+
+const validateRoute = (path) => {
+return path = resolve(path);
+
+}
+
+const mdLink = (path, options, err) => {
 // Retornaremos una promesa con la función
 
 return new Promise((resolve, reject) => {
-const data = fs.readFileSync(path, 'utf8');
-const tokens = marked.lexer(data);
-const html = marked.parser(tokens);
-const dom = new JSDOM(html);
-let links = dom.window.document.querySelectorAll('a');
+    if (err) reject()
+    else {
+    resolve();
+    const data = fs.readFileSync(path, 'utf8');
+    const tokens = marked.lexer(data);
+    const html = marked.parser(tokens);
+    const dom = new JSDOM(html);
+    let links = dom.window.document.querySelectorAll('a');
+    let dataLinks = {};
+    let arrayData = [];
+    for (let i = 0; i < links.length; i++) {
+        dataLinks = {
+        href : links[i].href,
+        text: links[i].textContent,
+        file: path
+      }
+      arrayData.push(dataLinks);
+      }
 
-let dataLinks = {};
-let arrayData = [];
-for (let i = 0; i < links.length; i++) {
-    dataLinks = {
-    href : links[i].href,
-    text: links[i].textContent,
-    file: path
+      console.log(arrayData);
   }
-
-  arrayData.push(dataLinks);
-  }
-
-  console.log(arrayData);
-
 });
 };
 
+
+
+
 module.exports = {
-  mdLink
+  mdLink,
+  validateRoute
 }
