fb87dbdf2339b9c5bc39ae5eeaec9d1e167c893a
Judith <baruki110214@gmail.com>
2018-09-05 19:41:20 -0500

    experimento match

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-const fs = require('fs');
-const path = require('path');
 
-const ruta = (data) => {
-  if (!path.isAbsolute(data)) {
+const match= url.match(/^https?:\/\/(www.\.[\w-]+\w{2,6})\/?/)
+console.log(match);
+set('protocol',match ? match[0])
 
-    const fileAbsolut = path.resolve(data);
-    fs.readFile(fileAbsolut, 'utf8', function(err, data) {
-      
-      if (err){
-        console.log('sin archivo');
-      } 
-      else{
-        const line = data.split('\n');
-        for (let i = 0; i < line.length; i++) {
-          const lines = line[i];
-          console.log(lines); }
-                                               }
-    });
-  }
-};
-
-ruta('./README.md');
+const readFileMd = () => {
+  fs.readFile('./README.md', 'utf8', (err, data) => {
+    if (err) {
+      console.log('error: ', err);
+    } else {
+      // console.log(data);
+      let regEx = /(http:\/\/|https:\/\/|www\.)[^\s][^)]+/g;
+      let newArray = data.match(regEx);
+      console.log(newArray);
