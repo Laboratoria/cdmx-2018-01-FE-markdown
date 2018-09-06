88fb8c07871e45a81cf67cb45230700d3bc2e7ad
Josce Jaimes <nanayhachi94@gmail>
2018-09-06 18:39:22 -0500

    haciendo funcion para registrar los links con lineCount

--- src/main.js
+++ src/main.js
@@ -4,37 +4,50 @@ const files = require('fs');
 //llamo a la api
 const path = require('path');
 
+const lineCount = (urlResult) =>{
+    files.readFile(urlResult,'utf8',(err, data) =>{
+  if (err){
+      console.log(err);
+      return 'error';
+  }else{
+      //saltos de linea \n
+      const lines = data.split('\n')
+        // console.log(lines);
+
+      lines.forEach(element => {
+
+      let expresion = new RegExp(/\[.{2,}\]\(.{2,}\)\W/g);
+      let newExpresion = element.match(expresion);        
+          console.log(newExpresion);
+          
+      });
+      // console.log(lines + ' ' +'lines')
+      // console.log(lines);
+      // return lines + ' '+'lines'
+           
+    }
+});
+}
+
 //definiendo una ruta absolutas
 const validatePath = (pathrout)=>{
     console.log(typeof pathrout);
     if(!path.isAbsolute(pathrout)) {
         const urlResult = path.resolve(pathrout)
-         console.log( urlResult);
+         //console.log( urlResult);
+         lineCount(urlResult)
     }
 }
 
-validatePath('README.md');
+validatePath('./src/README.md');
 
- const lineCount = (err, data) =>{
-    if (err){
-        console.log(err);
-        return 'error';
-    }else{
-        //saltos de linea \n
-        const lines = data.split('\n').length
-        console.log(data);
-        console.log(lines + ' ' +'lines')
-        return lines + ' '+'lines'
-        
-    };
-};
+ 
 
-files.readFile('README.md', 'utf8',lineCount)
+
 
 
 module.exports = {
     lineCount, 
     validatePath
 }; 
-
 
