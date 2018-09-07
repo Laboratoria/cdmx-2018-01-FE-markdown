5f2548f9324836ca990c21484299c5530da1858e
Josce Jaimes <nanayhachi94@gmail>
2018-09-07 16:28:19 -0500

    Haciendo extrallendo urls

--- src/main.js
+++ src/main.js
@@ -4,34 +4,51 @@ const files = require('fs');
 //llamo a la api
 const path = require('path');
 
+// const validate = (err, url) => {
+//     console.log(url);
+    
+// // if (err){
+// //     console.log('error');
+// // }else(url =  )
+
+// }
+
+const generateNewArrayUrl = (err,url) => {
+    //console.log(url);
+    
+}
+
+
 const lineCount = (urlResult) =>{
+    let newUrl = []; 
     files.readFile(urlResult,'utf8',(err, data) =>{
   if (err){
       console.log(err);
       return 'error';
   }else{
       //saltos de linea \n
-      const lines = data.split('\n')
-        // console.log(lines);
-
+      const lines = data.split('\n');
       lines.forEach(element => {
-
-      let expresion = new RegExp(/\[.{2,}\]\(.{2,}\)\W/g);
-      let newExpresion = element.match(expresion);        
-          console.log(newExpresion);
-          
-      });
-      // console.log(lines + ' ' +'lines')
-      // console.log(lines);
-      // return lines + ' '+'lines'
-           
+      const expresion = new RegExp(/(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g);
+      while(element.match(expresion) !== null){
+        newUrl.push(element.match(expresion));  
+        break; 
+         }        
+      });      
+      console.log(newUrl);
+      generateNewArrayUrl(newUrl)
+       return lines + ' '+'lines'     
     }
+    
 });
 }
 
+
+
+
 //definiendo una ruta absolutas
 const validatePath = (pathrout)=>{
-    console.log(typeof pathrout);
+    // console.log(typeof pathrout);
     if(!path.isAbsolute(pathrout)) {
         const urlResult = path.resolve(pathrout)
          //console.log( urlResult);
@@ -48,6 +65,7 @@ validatePath('./src/README.md');
 
 module.exports = {
     lineCount, 
-    validatePath
+    validatePath,
+    generateNewArrayUrl
 }; 
 
