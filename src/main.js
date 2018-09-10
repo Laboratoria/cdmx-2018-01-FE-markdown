102f0b206ad1a5ca644539144df4b9a60234ea45
Josceline Jaimes <39395887+JosceJaimes@users.noreply.github.com>
2018-09-10 14:46:53 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,36 +1,88 @@
-// aqui se va a implementar las funciones
+// aqui se va a implementar las funciones// aqui se va a implementar las funciones
 
 const files = require('fs');
+//llamo a la api
 const path = require('path');
 
+// const validate = (err, url) => {
+//     console.log(url);
+    
+// // if (err){
+// //     console.log('error');
+// // }else(url =  )
 
-const validateUrl = (url)=>{
-    if(path.isAbsolute(url) != true) {
-        console.log(url);
-        let urlResult = path.resolve(url)
-         console.log( urlResult);
-    }
-}
-validateUrl();
+// }
 
-files.readFile('README.md', 'utf8')
- const lineCount = (err, data) =>{
-    if (err){
-        console.log(err);
-        return 'error';
-    }else{
-        let lines = data.split('\n').length
-        console.log(lines + ' ' +'lines')
-        return lines + ' '+'lines'
+// const validateUrl = (err,newUrl) => {
+//     console.log(newUrl);
+    
+// }
+// console.log(newUrl);
+
+function validate(newUrl){
+   
+    
+    newUrl.forEach(element =>{
+        console.log(element);
+    //   fetch(element).then(function(response){
+    //       console.log(response.status);
+          
+
+      });
         
+    
+    
+// fetch(newUrl).then(response => console.log(response.status))
+
+}
+const lineCount = (urlResult) =>{
+    let newUrl = []; 
+    files.readFile(urlResult,'utf8',(err, data) =>{
+  if (err){
+      console.log(err);
+      return 'error';
+  }else{
+      //saltos de linea \n
+      const lines = data.split('\n');
+      lines.forEach(element => {
+      const expresion = new RegExp(/(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g);
+      while(element.match(expresion) !== null){  
+        newUrl.push(element.match(expresion));
+        return newUrl;
+         }              
+        }); 
+    //   console.log(newUrl);
+    //   validateUrl(newUrl);
+    validate(newUrl);
+       return lines + ' '+'lines'     
+    }
+    
+});
+}
+
+
+
+
+//definiendo una ruta absolutas
+const validatePath = (pathrout)=>{
+    // console.log(typeof pathrout);
+    if(!path.isAbsolute(pathrout)) {
+        const urlResult = path.resolve(pathrout)
+         //console.log( urlResult);
+         lineCount(urlResult)
     }
 }
-files.readFile('README.md', 'utf8',lineCount)
+
+validatePath('./README.md');
+
+ 
+
+
 
 
 module.exports = {
     lineCount, 
-    validateUrl
+    validatePath,
+    validate
 }; 
-
 
