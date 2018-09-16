971630bae920b0cfb1cf7809a8e23d0497d69901
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-09-16 10:43:53 -0500

    --validate y --stats funcionando. problemas al ejecutar md-links readme.md

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ const splitingRegex = (matchedRegEx, absolutePathReturn) =>{
   return newArrayFunction;
 };
 
-
 // function to obtain links' statistics 
 const validate = (url, urlText, path) =>{
   fetch(url).then((res=>{
@@ -70,82 +69,22 @@ const mdLinks = (path) =>{
   const readMd = mdFileReader(absolutePathReturn);
   const regexF = regExFunction(readMd);
   const newArray = splitingRegex(regexF, absolutePathReturn);
-  console.log(newArray);  
-  if (process.argv[3] === '--validate') {
-    validate(url, urlText, path);
+  if (process.argv[1] === `md-links` && process.argv[2] `${path}`) {
+    console.log(newArray);
   }
-  
+  newArray.forEach(element => {
+    const url = element.href;
+    const urlText = element.text;
+    if (process.argv[3] === '--validate') {
+      validate(url, urlText, path);
+    }
+  });
   if (process.argv[3] === '--stats') {
-    stats(matchedRegEx);
-  }
+    stats(regexF);
+  } 
 };
 
 module.exports = mdLinks;
-
-// const mdLinks = (path) => {
-//   const absolutePath = pathRequire.resolve(path);
-//   fs.readFile(absolutePath, 'utf8', (error, data) => {
-//     if (error) {
-//       console.log('error:', error);
-//     } else {
-//     //  we want regex to find "[text] (https://www.)" 
-//       let regEx = /\[([^\[\]]*)\]\(((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,}))\)/g;
-//       //  matchedLinksArray is matching the regex with the specific pieces of data.
-//       let matchedRegEx = data.match(regEx);
-//       const newArrayFunction = matchedRegEx.map(element => { 
-//       // split is separating the text and the href//  in que regex we are taking away "[]" and "()"" because we don't need those characters in our object "  // console.log(splittingElement) will show us 4 elements in each array of the newArray (2 elements are empty spaces).
-//         const splittingElement = element.split(/\[([^[\]]*)\]\(([^()]*)\)/g);
-//         const url = splittingElement[2];
-//         const urlText = splittingElement[1];
-//         // because i need to retun an array of objects, i´d declare an object with the obtained elements as values of the href and and the text properties
-//         const object = {
-//           href: url,
-//           text: urlText,
-//           file: absolutePath
-//         };
-//         if (process.argv[3] === '--validate') {
-//           validate(url, urlText, path);
-//         }
-//         return object;
-//       });
-//       if (process.argv[3] === '--stats') {
-//         stats(matchedRegEx);
-//       }
-//       console.log(newArrayFunction);    
-//     }
-//   });
-// };
-
-
-// // function to obtain links' statistics 
-// const validate = (url, urlText, path) =>{
-//   fetch(url).then((res=>{
-//     let linkStatus = res.status;
-//     let statusText = res.statusText;
-//     console.log(path, url, statusText, linkStatus, urlText);
-//   }
-//   ));
-// };
-
-// // function to obtain total of links and unique links
-// const stats = (matchedRegEx) =>{
-//   let totalUrls = matchedRegEx.length;
-//   let counterUnique = matchedRegEx.length;
-//   for (j = 0; j <= matchedRegEx.length; j++) { 
-//     for (i = 0; i <= matchedRegEx.length; i++) {
-//       if (i !== j && matchedRegEx[j] === matchedRegEx[i] && matchedRegEx[j].length === matchedRegEx[i].length) {
-//         counterUnique--;
-//       }
-//     }
-//   }
-//   console.log(`Total : ${totalUrls}
-// Unique : ${counterUnique}`);
-// };
-
-
-// module.exports = mdLinks; 
-
-// }
 /* readFile, método asíncrono, recibe 3 parámetros. 1 es la ruta del archivo que queremos leer, el segundo es un string con el juego de
      caracteres en el que el archivo está codificado y el 3ro es una callback que se ejecutará en el momento en el que el archivo esta leído
     y se encuentra disponible para hacer alguna cosa */
@@ -157,9 +96,3 @@ module.exports = mdLinks;
 //   }
 // });
 
-// promesa
-//   .then((absolutePath)=>{
-//     console.log((mdFileReader(absolutePath)));
-//   }) .catch((error) =>{
-//     console.log(error);
-//   });
